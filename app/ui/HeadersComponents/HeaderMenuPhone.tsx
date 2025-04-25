3812a81e592a4f58991a001d6b076bc9cde1886a
Smeto <smeto@vk.com>
2025-04-26 02:36:16 +0300

    Added picture mobile

--- app/ui/HeadersComponents/HeaderMenuPhone.tsx
+++ app/ui/HeadersComponents/HeaderMenuPhone.tsx
@@ -2,6 +2,7 @@ import { useModal } from "@/app/hooks/useModal";
 import { CrossSVG, DropDownMenuSVG } from "../SvgElements";
 import { BUTTON_BLACK } from "./HeaderNavPanel";
 import React, { useCallback, useEffect, useRef, useState } from "react";
+import Link from "next/link";
 
 const resetContainerHeight = (
   containerRef: React.RefObject<HTMLElement | null>
@@ -10,6 +11,14 @@ const resetContainerHeight = (
     containerRef.current.style.height = "";
   }
 };
+
+const MOBILE_MENU = [
+  { id: 1, title: "Для всех", type: "button", href: "/" },
+  { id: 2, title: "Private", type: "link", href: "2" },
+  { id: 3, title: "Бизнесу", type: "button", href: "3" },
+  { id: 4, title: "Финансовым организациям", type: "button", href: "5" },
+  { id: 5, title: "Инвесторам", type: "button", href: "5" },
+];
 
 export const HeaderMenuPhone = () => {
   const {
@@ -25,6 +34,8 @@ export const HeaderMenuPhone = () => {
     startHeight: 0,
     currentTranslateY: 0,
   });
+
+  const [activeItem, setActiveItem] = useState(MOBILE_MENU[0].id);
 
   const DEBUG_Block_Visualization =
     process.env.NODE_ENV === "development"
@@ -58,11 +69,10 @@ export const HeaderMenuPhone = () => {
     return () => clearTimeout(timer);
   }, [toggleModalDropDown]);
 
-  //Обрабатываем движение мыши
-  const handleMouseMove = useCallback(
-    (e: MouseEvent) => {
+  const handleMove = useCallback(
+    (clientY: number) => {
       if (!isDragging || !containerRef.current) return;
-      const deltaY = e.clientY - dragInfoRef.current.startY;
+      const deltaY = clientY - dragInfoRef.current.startY;
       const newHeight = dragInfoRef.current.startHeight - deltaY;
       const minHeight = 40;
 
@@ -71,23 +81,30 @@ export const HeaderMenuPhone = () => {
         setIsDragging(false);
         return;
       }
-
       containerRef.current.style.height = `${newHeight}px`;
     },
     [isDragging, handleClose]
   );
 
-  //Обрабатываем если мышь отпустили
-  const handleMouseUp = useCallback(() => {
+  //Обрабатываем движение мыши
+  const handleMouseMove = useCallback(
+    (e: MouseEvent) => {
+      handleMove(e.clientY);
+    },
+    [handleMove]
+  );
+
+  //Обрабатываем если отпустили
+  const handleEnd = useCallback(() => {
     setIsDragging(false);
     resetContainerHeight(containerRef);
   }, []);
 
   //Обработчик начала перетаскивания
-  const handleMouseDown = useCallback((e: React.MouseEvent) => {
+  const handleStart = useCallback((clientY: number) => {
     if (containerRef.current) {
       dragInfoRef.current = {
-        startY: e.clientY,
+        startY: clientY,
         startHeight: containerRef.current.getBoundingClientRect().height,
         currentTranslateY: 0,
       };
@@ -95,17 +112,41 @@ export const HeaderMenuPhone = () => {
     }
   }, []);
 
+  const handleMouseDown = useCallback(
+    (e: React.MouseEvent) => {
+      handleStart(e.clientY);
+    },
+    [handleStart]
+  );
+  const handleTouchStart = useCallback(
+    (e: React.TouchEvent) => {
+      handleStart(e.touches[0].clientY);
+    },
+    [handleStart]
+  );
+
+  const handleTouchMove = useCallback(
+    (e: TouchEvent) => {
+      handleMove(e.touches[0].clientY);
+    },
+    [handleMove]
+  );
+
   useEffect(() => {
     if (isDragging) {
       document.addEventListener("mousemove", handleMouseMove);
-      document.addEventListener("mouseup", handleMouseUp);
+      document.addEventListener("mouseup", handleEnd);
+      document.addEventListener("touchmove", handleTouchMove);
+      document.addEventListener("touchend", handleEnd);
     }
 
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
+      document.removeEventListener("mouseup", handleEnd);
+      document.removeEventListener("touchmove", handleTouchMove);
+      document.removeEventListener("touchend", handleEnd);
     };
-  }, [isDragging, handleMouseMove, handleMouseUp]);
+  }, [isDragging, handleMouseMove, handleEnd, handleTouchMove]);
 
   return (
     <>
@@ -129,6 +170,7 @@ export const HeaderMenuPhone = () => {
         <div
           className={`relative min-h-14 cursor-ns-resize ${DEBUG_Block_Visualization}`}
           onMouseDown={handleMouseDown}
+          onTouchStart={handleTouchStart}
         >
           <div className="absolute top-0 left-0 right-0 h-2 bg-transparent" />
 
@@ -141,6 +183,65 @@ export const HeaderMenuPhone = () => {
               <div className="flex h-4 w-4">
                 <CrossSVG />
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="pl-4 pr-4">
+          <div className="pt-6 pb-8.5 min-h-100">
+            <div
+              className="flex p-1 gap-0.5 border-[1px] border-solid border-[#dbdfec] w-full overflow-x-auto rounded-xl"
+              style={{ scrollbarWidth: "none" }}
+            >
+              {MOBILE_MENU.map((item) => (
+                <div
+                  key={item.id}
+                  className={`flex-shrink-0 text-[16px] leading-5 text-weig font-medium h-12 pl-6 pr-6 flex items-center cursor-pointer ${
+                    activeItem === item.id
+                      ? "bg-black text-white rounded-xl"
+                      : ""
+                  }`}
+                  onClick={() => setActiveItem(item.id)}
+                >
+                  {item.type === "button" ? (
+                    <button className="cursor-pointer">{item.title}</button>
+                  ) : (
+                    <Link href={"#"}>{item.title}</Link>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="pt-8.5">
+              <Link href={"#"}>
+                <div className="bg-[rgb(244,246,255)] p-4 rounded-2xl h-20 flex relative">
+                  <div className="max-w-[60%] min-w-50 ">
+                    <div className="text-[16px] font-medium pb-1 leading-5">
+                      Офиса и банкоматы
+                    </div>
+                    <div className="text-[14px] font-normal leading-4">
+                      Найдите ближайшее отделение
+                    </div>
+                  </div>
+                  <div className="flex items-center ml-auto">
+                    <picture>
+                      <source
+                        type="image/webp"
+                        srcSet="https://cdn.gpb.ru/upload/files/iblock/f3a/qe8p1flth1k7bnfhssg5ys5mwgq1ek59/offices_atms.webp 2x, https://cdn.gpb.ru/upload/files/iblock/f3a/qe8p1flth1k7bnfhssg5ys5mwgq1ek59/x1_offices_atms.webp"
+                      />
+                      <source
+                        type="image/png"
+                        srcSet="https://cdn.gpb.ru/upload/files/iblock/f3a/qe8p1flth1k7bnfhssg5ys5mwgq1ek59/offices_atms.png 2x, https://cdn.gpb.ru/upload/files/iblock/f3a/qe8p1flth1k7bnfhssg5ys5mwgq1ek59/x1_offices_atms.png"
+                      />
+                      <img
+                        className="absolute max-w-37 h-23 bottom-0 right-0"
+                        src="https://cdn.gpb.ru/upload/files/iblock/f3a/qe8p1flth1k7bnfhssg5ys5mwgq1ek59/x1_offices_atms.png"
+                        alt="Офисы и банкоматы"
+                      />
+                    </picture>
+                  </div>
+                </div>
+              </Link>
             </div>
           </div>
         </div>

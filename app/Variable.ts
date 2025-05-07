33f03af40a30841c78129ef1146a1a3fcb689197
Aleksand Potapenkov <smeto@vk.com>
2025-05-08 01:37:04 +0300

    Selector F Products

---
+++ app/Variable.ts
@@ -0,0 +1,82 @@
+"use client";
+
+export const FINANCIAL_PRODUCTS_ITEMS = [
+  {
+    id: 1,
+    href: "#",
+    title: "Накопительный счет ",
+    description: "Повышенная ставка новым клиентам на 2 месяца",
+    bg: "#476bf0", //blue
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/5a6/1k5guj918vstcc3fnu2f2xhk5bvlexgo/Head-banner-Desktop-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/5a6/1k5guj918vstcc3fnu2f2xhk5bvlexgo/x1_Head-banner-Desktop-960x1120px.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/5a6/1k5guj918vstcc3fnu2f2xhk5bvlexgo/Head-banner-Desktop-960x1120px.png 2x, https://cdn.gpb.ru/upload/files/bve/5a6/1k5guj918vstcc3fnu2f2xhk5bvlexgo/x1_Head-banner-Desktop-960x1120px.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/5a6/1k5guj918vstcc3fnu2f2xhk5bvlexgo/x1_Head-banner-Desktop-960x1120px.png",
+  },
+  {
+    id: 2,
+    href: "#",
+    title: "Кэшбэк на самое важное",
+    description: "По дебетовой карте",
+    bg: "#ddf1ff", //teal
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/699/a094p509kwihmwyq2c09lt6qmf7fuh1k/Head-banner-Desktop-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/699/a094p509kwihmwyq2c09lt6qmf7fuh1k/x1_Head-banner-Desktop-960x1120px.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/699/a094p509kwihmwyq2c09lt6qmf7fuh1k/Head-banner-Desktop-960x1120px.png 2x, https://cdn.gpb.ru/upload/files/bve/699/a094p509kwihmwyq2c09lt6qmf7fuh1k/x1_Head-banner-Desktop-960x1120px.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/699/a094p509kwihmwyq2c09lt6qmf7fuh1k/x1_Head-banner-Desktop-960x1120px.png",
+  },
+  {
+    id: 3,
+    href: "#",
+    title: "Программа долгосрочных сбережений",
+    description: "До 64% выгоды к личным взносам по программе",
+    bg: "#ffd7c4", //orange
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/df6/9orh0nxl6exk1c9ndf100xqed4oetkjs/Head-banner-Desktop-_-PDS-_-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/df6/9orh0nxl6exk1c9ndf100xqed4oetkjs/x1_Head-banner-Desktop-_-PDS-_-960x1120px.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/df6/9orh0nxl6exk1c9ndf100xqed4oetkjs/Head-banner-Desktop-_-PDS-_-960x1120px.png 2x, https://cdn.gpb.ru/upload/files/bve/df6/9orh0nxl6exk1c9ndf100xqed4oetkjs/x1_Head-banner-Desktop-_-PDS-_-960x1120px.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/df6/9orh0nxl6exk1c9ndf100xqed4oetkjs/x1_Head-banner-Desktop-_-PDS-_-960x1120px.png",
+  },
+  {
+    id: 4,
+    href: "#",
+    title: "Лучшая премиальная карта",
+    description: "с выгодой от 500 000 ₽ в год",
+    bg: "#4d2331", //cherry
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/911/f951dz6nfpw8bztxuojcl35rqd2vkbw7/Head-banner-Desktop-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/911/f951dz6nfpw8bztxuojcl35rqd2vkbw7/x1_Head-banner-Desktop-960x1120px.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/911/f951dz6nfpw8bztxuojcl35rqd2vkbw7/Head-banner-Desktop-960x1120px.png 2x, https://cdn.gpb.ru/upload/files/bve/911/f951dz6nfpw8bztxuojcl35rqd2vkbw7/x1_Head-banner-Desktop-960x1120px.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/911/f951dz6nfpw8bztxuojcl35rqd2vkbw7/x1_Head-banner-Desktop-960x1120px.png",
+  },
+  {
+    id: 5,
+    href: "#",
+    title: "Большой кредит с низким платежом",
+    description: "Выгоднее с залогом недвижимости",
+    bg: "#a8dcff", // light blue
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/173/exuq6hvwwpgot1boknxp8i1gc1kc3pvs/Head-banner-Desktop-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/173/exuq6hvwwpgot1boknxp8i1gc1kc3pvs/x1_Head-banner-Desktop-960x1120px.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/173/exuq6hvwwpgot1boknxp8i1gc1kc3pvs/Head-banner-Desktop-960x1120px.png 2x, https://cdn.gpb.ru/upload/files/bve/173/exuq6hvwwpgot1boknxp8i1gc1kc3pvs/x1_Head-banner-Desktop-960x1120px.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/173/exuq6hvwwpgot1boknxp8i1gc1kc3pvs/x1_Head-banner-Desktop-960x1120px.png",
+  },
+  {
+    id: 6,
+    href: "#",
+    title: "Драгоценные металлы",
+    description: "Покупка и продажа золотых слитков",
+    bg: "#3356d7", //blue
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/Head-banner-Desktop-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/x1_Head-banner-Desktop-960x1120px.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/Head-banner-Desktop-960x1120px.png 2x, https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/x1_Head-banner-Desktop-960x1120px.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/x1_Head-banner-Desktop-960x1120px.png",
+  },
+];
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,19 @@
+"use client";
+import { FINANCIAL_PRODUCTS_ITEMS } from "./Variable";
+
 export default function Home() {
-  return <div></div>;
+  console.log("Загруженно Home");
+  return (
+    <div>
+      <div className="max-[1023]:max-w-3xl max-[1023]:ml-auto max-[1023]:mr-auto">
+        <section>
+          {FINANCIAL_PRODUCTS_ITEMS.map((item) => (
+            <div key={item.id}>
+              <h3>{item.title}</h3>
+            </div>
+          ))}
+        </section>
+      </div>
+    </div>
+  );
 }

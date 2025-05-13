3ca7fcde3ca0ab8ab70457ba29d64e2a3751af3a
Smeto <smeto@vk.com>
2025-05-13 04:29:35 +0300

    useful tip items

--- app/Variable.ts
+++ app/Variable.ts
@@ -19,8 +19,10 @@ export const FINANCIAL_PRODUCTS_ITEMS = [
   {
     id: 2,
     href: "#",
-    title: "Кэшбэк на самое важное",
+    title: "Карта с кэшбэком на самое важное",
     description: "По дебетовой карте",
+    titleColor: "#000000",
+    ArrowColor: "#ffffff",
     bg: "#ddf1ff", //teal
     srcsetWebp:
       "https://cdn.gpb.ru/upload/files/bve/3f8/d54nohkxpwx56jczcs5nffcg07rjhi74/Mobile-new-_-280kh360_1_.webp 2x, https://cdn.gpb.ru/upload/files/bve/3f8/d54nohkxpwx56jczcs5nffcg07rjhi74/x1_Mobile-new-_-280kh360_1_.webp",
@@ -34,6 +36,8 @@ export const FINANCIAL_PRODUCTS_ITEMS = [
     href: "#",
     title: "Программа долгосрочных сбережений",
     description: "До 64% выгоды к личным взносам по программе",
+    titleColor: "#000000",
+    ArrowColor: "#ffffff",
     bg: "#ffd7c4", //orange
     srcsetWebp:
       "https://cdn.gpb.ru/upload/files/bve/4c0/s70p3jejxbj9nq80y59oowye46o3ctig/Head-banner-mobile-NEW-_-PDS.webp 2x, https://cdn.gpb.ru/upload/files/bve/4c0/s70p3jejxbj9nq80y59oowye46o3ctig/x1_Head-banner-mobile-NEW-_-PDS.webp",
@@ -52,11 +56,15 @@ export const FINANCIAL_PRODUCTS_ITEMS = [
     id: 4,
     href: "#",
     title: "Лучшая премиальная карта",
+    titleColor: "#ffffff",
     description: "с выгодой от 500 000 ₽ в год",
     bg: "#4d2331", //cherry
-    srcsetWebp: "",
-    srcsetPng: "",
-    srcImg: "",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/b6c/li6wzfdtjc50c1j0u5ammyawbxv5d4kf/Mobile-new-_-280kh360.webp 2x, https://cdn.gpb.ru/upload/files/bve/b6c/li6wzfdtjc50c1j0u5ammyawbxv5d4kf/x1_Mobile-new-_-280kh360.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/b6c/li6wzfdtjc50c1j0u5ammyawbxv5d4kf/Mobile-new-_-280kh360.png 2x, https://cdn.gpb.ru/upload/files/bve/b6c/li6wzfdtjc50c1j0u5ammyawbxv5d4kf/x1_Mobile-new-_-280kh360.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/b6c/li6wzfdtjc50c1j0u5ammyawbxv5d4kf/x1_Mobile-new-_-280kh360.png",
     /*srcsetWebp:
       "https://cdn.gpb.ru/upload/files/bve/911/f951dz6nfpw8bztxuojcl35rqd2vkbw7/Head-banner-Desktop-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/911/f951dz6nfpw8bztxuojcl35rqd2vkbw7/x1_Head-banner-Desktop-960x1120px.webp",
     srcsetPng:
@@ -69,10 +77,15 @@ export const FINANCIAL_PRODUCTS_ITEMS = [
     href: "#",
     title: "Большой кредит с низким платежом",
     description: "Выгоднее с залогом недвижимости",
+    titleColor: "#000000",
+    ArrowColor: "#ffffff",
     bg: "#a8dcff", // light blue
-    srcsetWebp: "",
-    srcsetPng: "",
-    srcImg: "",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/1e3/58xppndm9rb73gzqkkeiluzl1t8d2p6f/5.-bez-NG_New-head-banner-mobile_KPZN_280kh360.webp 2x, https://cdn.gpb.ru/upload/files/bve/1e3/58xppndm9rb73gzqkkeiluzl1t8d2p6f/x1_5.-bez-NG_New-head-banner-mobile_KPZN_280kh360.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/1e3/58xppndm9rb73gzqkkeiluzl1t8d2p6f/5.-bez-NG_New-head-banner-mobile_KPZN_280kh360.png 2x, https://cdn.gpb.ru/upload/files/bve/1e3/58xppndm9rb73gzqkkeiluzl1t8d2p6f/x1_5.-bez-NG_New-head-banner-mobile_KPZN_280kh360.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/1e3/58xppndm9rb73gzqkkeiluzl1t8d2p6f/x1_5.-bez-NG_New-head-banner-mobile_KPZN_280kh360.png",
     /*srcsetWebp:
       "https://cdn.gpb.ru/upload/files/bve/173/exuq6hvwwpgot1boknxp8i1gc1kc3pvs/Head-banner-Desktop-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/173/exuq6hvwwpgot1boknxp8i1gc1kc3pvs/x1_Head-banner-Desktop-960x1120px.webp",
     srcsetPng:
@@ -84,11 +97,15 @@ export const FINANCIAL_PRODUCTS_ITEMS = [
     id: 6,
     href: "#",
     title: "Драгоценные металлы",
+    titleColor: "#ffffff",
     description: "Покупка и продажа золотых слитков",
     bg: "#3356d7", //blue
-    srcsetWebp: "",
-    srcsetPng: "",
-    srcImg: "",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/bve/768/unx71tq30yiqk3hfmfcrfxa6dnn67s4g/6.-bez-NG_Mobile-haed-banner-_NEW_-_-drag-metal_-280x360px.webp 2x, https://cdn.gpb.ru/upload/files/bve/768/unx71tq30yiqk3hfmfcrfxa6dnn67s4g/x1_6.-bez-NG_Mobile-haed-banner-_NEW_-_-drag-metal_-280x360px.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/bve/768/unx71tq30yiqk3hfmfcrfxa6dnn67s4g/6.-bez-NG_Mobile-haed-banner-_NEW_-_-drag-metal_-280x360px.png 2x, https://cdn.gpb.ru/upload/files/bve/768/unx71tq30yiqk3hfmfcrfxa6dnn67s4g/x1_6.-bez-NG_Mobile-haed-banner-_NEW_-_-drag-metal_-280x360px.png",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/bve/768/unx71tq30yiqk3hfmfcrfxa6dnn67s4g/x1_6.-bez-NG_Mobile-haed-banner-_NEW_-_-drag-metal_-280x360px.png",
     /*
     srcsetWebp:
       "https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/Head-banner-Desktop-960x1120px.webp 2x, https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/x1_Head-banner-Desktop-960x1120px.webp",
@@ -96,5 +113,78 @@ export const FINANCIAL_PRODUCTS_ITEMS = [
       "https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/Head-banner-Desktop-960x1120px.png 2x, https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/x1_Head-banner-Desktop-960x1120px.png",
     srcImg:
       "https://cdn.gpb.ru/upload/files/bve/4bb/sqguii0x9ybhc7a4tp0dptfzvb00x55z/x1_Head-banner-Desktop-960x1120px.png",*/
+  },
+];
+
+export const USESFUL_TIPS = [
+  {
+    id: 1,
+    title: "Страховка карты",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/iblock/469/8yshc0gjw007syumt4ln8n6t3d9p5jml/Ikonka_.webp 2x, https://cdn.gpb.ru/upload/files/iblock/469/8yshc0gjw007syumt4ln8n6t3d9p5jml/x1_Ikonka_.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/iblock/469/8yshc0gjw007syumt4ln8n6t3d9p5jml/Ikonka_.jpg 2x, https://cdn.gpb.ru/upload/files/iblock/469/8yshc0gjw007syumt4ln8n6t3d9p5jml/x1_Ikonka_.jpg",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/iblock/469/8yshc0gjw007syumt4ln8n6t3d9p5jml/x1_Ikonka_.jpg",
+  },
+  {
+    id: 2,
+    title: "Аренда без риелтора",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/iblock/b0e/i5xdw6iii64u0c7685ce2ihio2hb4zwf/Banner-na-sayt-_1_-_2_.webp 2x, https://cdn.gpb.ru/upload/files/iblock/b0e/i5xdw6iii64u0c7685ce2ihio2hb4zwf/x1_Banner-na-sayt-_1_-_2_.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/iblock/b0e/i5xdw6iii64u0c7685ce2ihio2hb4zwf/Banner-na-sayt-_1_-_2_.jpg 2x, https://cdn.gpb.ru/upload/files/iblock/b0e/i5xdw6iii64u0c7685ce2ihio2hb4zwf/x1_Banner-na-sayt-_1_-_2_.jpg",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/iblock/b0e/i5xdw6iii64u0c7685ce2ihio2hb4zwf/x1_Banner-na-sayt-_1_-_2_.jpg",
+  },
+  {
+    id: 3,
+    title: "Копим на будущее",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/iblock/cd6/a7nnndqkxran111x0ohfqyrkhqe9v6uj/Banner-na-sayt-0-_1_.webp 2x, https://cdn.gpb.ru/upload/files/iblock/cd6/a7nnndqkxran111x0ohfqyrkhqe9v6uj/x1_Banner-na-sayt-0-_1_.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/iblock/cd6/a7nnndqkxran111x0ohfqyrkhqe9v6uj/Banner-na-sayt-0-_1_.jpg 2x, https://cdn.gpb.ru/upload/files/iblock/cd6/a7nnndqkxran111x0ohfqyrkhqe9v6uj/x1_Banner-na-sayt-0-_1_.jpg",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/iblock/cd6/a7nnndqkxran111x0ohfqyrkhqe9v6uj/x1_Banner-na-sayt-0-_1_.jpg",
+  },
+  {
+    id: 4,
+    title: "Маршрут онлайн",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/iblock/d91/iy9i7vylhnr5sz4pxgni0yihbzhboxvw/GorodPey-ikonka-_1_.webp 2x, https://cdn.gpb.ru/upload/files/iblock/d91/iy9i7vylhnr5sz4pxgni0yihbzhboxvw/x1_GorodPey-ikonka-_1_.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/iblock/d91/iy9i7vylhnr5sz4pxgni0yihbzhboxvw/GorodPey-ikonka-_1_.jpg 2x, https://cdn.gpb.ru/upload/files/iblock/d91/iy9i7vylhnr5sz4pxgni0yihbzhboxvw/x1_GorodPey-ikonka-_1_.jpg",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/iblock/d91/iy9i7vylhnr5sz4pxgni0yihbzhboxvw/x1_GorodPey-ikonka-_1_.jpg",
+  },
+  {
+    id: 5,
+    title: "Как защитить средства",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/iblock/037/2txtmvqkrwlze8o6n48b3gctlbqrkbgp/Banner-na-sayt-_16_.webp 2x, https://cdn.gpb.ru/upload/files/iblock/037/2txtmvqkrwlze8o6n48b3gctlbqrkbgp/x1_Banner-na-sayt-_16_.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/iblock/037/2txtmvqkrwlze8o6n48b3gctlbqrkbgp/Banner-na-sayt-_16_.jpg 2x, https://cdn.gpb.ru/upload/files/iblock/037/2txtmvqkrwlze8o6n48b3gctlbqrkbgp/x1_Banner-na-sayt-_16_.jpg",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/iblock/037/2txtmvqkrwlze8o6n48b3gctlbqrkbgp/x1_Banner-na-sayt-_16_.jpg",
+  },
+  {
+    id: 6,
+    title: "Мифы о самозанятых",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/iblock/c4a/8tjx1vm5x7qi2k6hs2xoiz46gnynnhb4/Banner-na-sayt-_17_.webp 2x, https://cdn.gpb.ru/upload/files/iblock/c4a/8tjx1vm5x7qi2k6hs2xoiz46gnynnhb4/x1_Banner-na-sayt-_17_.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/iblock/c4a/8tjx1vm5x7qi2k6hs2xoiz46gnynnhb4/Banner-na-sayt-_17_.jpg 2x, https://cdn.gpb.ru/upload/files/iblock/c4a/8tjx1vm5x7qi2k6hs2xoiz46gnynnhb4/x1_Banner-na-sayt-_17_.jpg",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/iblock/c4a/8tjx1vm5x7qi2k6hs2xoiz46gnynnhb4/x1_Banner-na-sayt-_17_.jpg",
+  },
+  {
+    id: 7,
+    title: "Виды бюджета",
+    srcsetWebp:
+      "https://cdn.gpb.ru/upload/files/iblock/7f6/mac0dq911f6dgsnn0ov4esj3ects66no/Banner-na-sayt-_12_.webp 2x, https://cdn.gpb.ru/upload/files/iblock/7f6/mac0dq911f6dgsnn0ov4esj3ects66no/x1_Banner-na-sayt-_12_.webp",
+    srcsetPng:
+      "https://cdn.gpb.ru/upload/files/iblock/7f6/mac0dq911f6dgsnn0ov4esj3ects66no/Banner-na-sayt-_12_.jpg 2x, https://cdn.gpb.ru/upload/files/iblock/7f6/mac0dq911f6dgsnn0ov4esj3ects66no/x1_Banner-na-sayt-_12_.jpg",
+    srcImg:
+      "https://cdn.gpb.ru/upload/files/iblock/7f6/mac0dq911f6dgsnn0ov4esj3ects66no/x1_Banner-na-sayt-_12_.jpg",
   },
 ];
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { FINANCIAL_PRODUCTS_ITEMS } from "./Variable";
+import { FINANCIAL_PRODUCTS_ITEMS, USESFUL_TIPS } from "./Variable";
 import { ArrowSVG } from "./ui/SvgElements";
 
 export default function Home() {
@@ -8,20 +8,20 @@ export default function Home() {
   return (
     <div>
       <div className="max-[1023]:max-w-3xl max-[1023]:ml-auto max-[1023]:mr-auto min-[1024]:hidden">
-        <section className="flex h-45 pl-4 pr-4">
+        <section className="flex h-45 pl-4 pr-4 gap-4 overflow-x-auto overflow-y-hidden scrollbar-hide">
           {FINANCIAL_PRODUCTS_ITEMS.map((item) => (
             <Link
               key={item.id}
               href={item.href}
-              className="relative min-[360]:w-80 rounded-2xl w-[calc(100vw-40px)] "
+              className="relative block flex-shrink-0 min-[360]:w-80 rounded-2xl w-[calc(100vw-40px)] h-full"
               style={{ backgroundColor: item.bg }}
             >
-              <span className="flex w-35 ml-auto items-center">
+              <span className="block w-35 ml-auto items-center ">
                 <picture className="">
                   <source type="image/webp" srcSet={item.srcsetWebp} />
                   <source type="image/png" srcSet={item.srcsetPng} />
                   <img
-                    className="max-[1023px]:w-35 object-contain"
+                    className="max-[1023px]:w-35 object-contain h-[170px]"
                     src={item.srcImg}
                     alt={item.title}
                   />
@@ -29,7 +29,7 @@ export default function Home() {
               </span>
               <span className="absolute top-0 left-0 w-full h-full p-5">
                 <h3
-                  className="max-[1023]:text-[23px] max-[1023]:leading-7 font-semibold max-w-40 "
+                  className="max-[1023]:text-[23px] max-[1023]:leading-7 font-semibold max-w-46 "
                   style={{ color: item?.titleColor }}
                 >
                   {item.title}
@@ -37,9 +37,12 @@ export default function Home() {
                 <p className="hidden" style={{ color: item?.titleColor }}>
                   {item.description}
                 </p>
-                <button className="absolute flex w-8 h-8 bottom-5 left-5 bg-white items-center justify-center rounded-[12px] hover:bg-[#E2E2E5] cursor-pointer transition-colors duration-200">
+                <button
+                  className="absolute flex w-8 h-8 bottom-5 left-5 items-center justify-center rounded-[12px] hover:bg-[#E2E2E5] cursor-pointer transition-colors duration-200"
+                  style={{ background: item?.titleColor }}
+                >
                   <div
-                    className="flex h-4 w-4  "
+                    className="flex h-4 w-4 "
                     style={{ color: item?.ArrowColor }}
                   >
                     <ArrowSVG />
@@ -48,6 +51,35 @@ export default function Home() {
               </span>
             </Link>
           ))}
+        </section>
+        <div className="pb-8" />
+        <section className="ml-auto mr-auto w-full pl-4">
+          {" "}
+          {/* Добавляем горизонтальные отступы */}
+          <div className="flex gap-3.5 overflow-x-auto overflow-y-hidden scrollbar-hide w-full">
+            {USESFUL_TIPS.map((item) => (
+              <div
+                key={item.id}
+                className="relative flex-shrink-0 flex items-center justify-center min-w-24 w-24 h-27 cursor-pointer" // Добавляем flex-shrink-0
+              >
+                <div className="flex">
+                  <picture>
+                    <source type="image/webp" srcSet={item.srcsetWebp} />
+                    <source type="image/png" srcSet={item.srcsetPng} />
+                    <img
+                      className="rounded-xl"
+                      src={item.srcImg}
+                      alt={item.title}
+                    />
+                  </picture>
+                </div>
+                <div className="absolute rounded-xl w-22 h-25 bg-[rgba(30,34,46,0.32)]" />
+                <p className="font-semibold text-[12px] leading-4 absolute top-4 w-19 text-white">
+                  {item.title}
+                </p>
+              </div>
+            ))}
+          </div>
         </section>
       </div>
     </div>
